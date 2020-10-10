8cd95bc783796c7b233abb5ade6cdb17e85dbd69
Loriya Anas <48900508+anas1606@users.noreply.github.com>
2020-10-10 16:21:27 +0530

    Add files via upload

---
+++ Boy or Girl/solution.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    string str;
+    int cnt = 0,len,ar[26] = {0};
+    cin >> str;
+    len = str.size();
+    for(int i = 0; i < len; i++){
+
+        if(ar[str[i] - 'a'] == 0){
+            cnt++;
+            ar[str[i] - 'a'] = 1;
+        }
+    }
+    if(!(cnt%2))cout << "CHAT WITH HER!" << endl;
+    else cout << "IGNORE HIM!" << endl;
+
+    return 0;
+}
